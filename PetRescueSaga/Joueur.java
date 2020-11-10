754019649b840a27951774287573338f39cc8b13
CLIPAL moise <elymo.mc@gmail.com>
2020-11-10 19:17:07 +0100

    Création d'un constructeur Joueur.

--- PetRescueSaga/Joueur.java
+++ PetRescueSaga/Joueur.java
@@ -2,4 +2,12 @@ public class Joueur {
     String nom;
     int vie;
     int argent;
+
+    protected Joueur(String nom, int vie, int argent ){
+        this.nom = nom;
+        this.vie = vie;
+        this.argent = argent;
+    }
+
+
 }
