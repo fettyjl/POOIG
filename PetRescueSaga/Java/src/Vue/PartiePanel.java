fdc199af1d9492198715481907b07220662bfadd
Jylan Payet <jylanpayet@gmail.com>
2020-12-30 00:18:59 +0100

    fetty

--- PetRescueSaga/Java/src/Vue/PartiePanel.java
+++ PetRescueSaga/Java/src/Vue/PartiePanel.java
@@ -15,21 +15,20 @@ public class PartiePanel extends ImagePanel {
         int index = 0;
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(3, 3, 3, 3);
+        gbc.insets = new Insets(1, 1, 1, 1);
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 Color color;
                     if(fenetre.part.p.plateau[row][col].container instanceof Bloc){
                         Bloc a=(Bloc) fenetre.part.p.plateau[row][col].container;
                         if(a.i == 0){
-                            color=new Color(0,0,255);
+                            color=new Color( 41, 128, 185 );
                         }else if(a.i == 1){
-                            color=new Color(255,0,0);
+                            color=new Color( 231, 76, 60 );
                         }else if(a.i==2){
-                            color=new Color(0,255,0);
+                            color=new Color( 244, 208, 63);
                         }else{
-                            color=new Color(255,255,0);
-
+                            color=new Color(  39, 174, 96);
                         }
                         gbc.gridx = col;
                         gbc.gridy = row;
@@ -51,8 +50,6 @@ public class PartiePanel extends ImagePanel {
             setBorderPainted(false);
             setBackground(background);
             setOpaque(true);
-
-
         }
 
         @Override
