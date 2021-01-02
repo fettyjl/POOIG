f319499158d5e05a1b9525b48284f895d9083d91
CLIPAL moise <elymo.mc@gmail.com>
2021-01-02 23:06:25 +0100

    OK2

--- PetRescueSaga/Java/src/Jeu/Game.java
+++ PetRescueSaga/Java/src/Jeu/Game.java
@@ -37,9 +37,66 @@ public class Game {
                         System.out.println("Sac d'argent sauvés :" + choix.argentSave + "/4");
                         System.out.println("Sac perdu :" + choix.argentPerdu + "/4");
                         System.out.println("Bonus Peinture " + this.joueur.bonus.peinture + "/ Fusée " + this.joueur.bonus.fusee + "/ Sauvetage " + this.joueur.bonus.sauvetage);
-                        //Donner la possibilité au Joueur d'utiliser les bonus TODO
 
                         choix.plateau.afficherPlateau();
+
+                        do{
+                            System.out.println("Voules utiliser un bonus ? (Y/N)");
+                            String yn = sc.next();
+                            if (yn.equals("Y")){
+                                System.out.println("quel bonus veux tu utiliser ? ");
+                                System.out.println("Bonus Peinture (P) " + this.joueur.bonus.peinture + "/ Fusée (F) " + this.joueur.bonus.fusee + "/ Sauvetage (S)" + this.joueur.bonus.sauvetage);
+                                 do{
+                                     System.out.print("choisir entre (P)/(F) ou (S) ");
+                                     String pfs = sc.next();
+                                     switch (pfs) {
+
+                                         case "P" -> {
+                                             System.out.println("Case à jouer pour bonus Peinture ? (x,y)");
+                                             Container a;
+                                             int q,z;
+                                             do {
+                                                 System.out.println("Quel x ?");
+                                                 q = sc.nextInt();
+                                                 System.out.println("Quel y ?");
+                                                 z = sc.nextInt();
+                                                 a = choix.plateau.plateau[q][z].container;
+                                             } while ((q > -1 && q > choix.plateau.longueur) || (z > -1 && z > choix.plateau.largeur) && !(a instanceof Bloc) );
+                                             Bloc b = (Bloc) a;
+                                             b = b.changementCouleur();
+                                             choix.plateau.refreshPlateau();
+                                             choix.plateau.afficherPlateau();
+
+                                         }
+                                         case "F" -> {
+                                             System.out.println("Case à jouer pour bonus Fusée ? (x,y)");
+                                             do {
+                                                 System.out.println("Quel x ?");
+                                                 x = sc.nextInt();
+                                                 System.out.println("Quel y ?");
+                                                 y = sc.nextInt();
+                                             } while ((x > -1 && x > choix.plateau.longueur) || (y > -1 && y > choix.plateau.largeur));
+                                         }
+                                         case "S" -> {
+                                             System.out.println("Case à jouer pour bonus Sauvetage ? (x,y)");
+                                             do {
+                                                 System.out.println("Quel x ?");
+                                                 x = sc.nextInt();
+                                                 System.out.println("Quel y ?");
+                                                 y = sc.nextInt();
+                                             } while ((x > -1 && x > choix.plateau.longueur) || (y > -1 && y > choix.plateau.largeur));
+                                         }
+                                         default -> System.out.println("FAIRE UN CHOIX ENTRE (P) (F) (S)");
+                                     }
+
+                                 }while (sc.next().charAt(0) != 'P' || sc.next().charAt(0) != 'F' || sc.next().charAt(0) != 'S' );
+
+                            } else {
+
+                            }
+                        } while(sc.next().charAt(0) != 'Y' || sc.next().charAt(0) != 'N');
+
+
 
                         System.out.println("Case à jouer (x,y)");
                         do {
