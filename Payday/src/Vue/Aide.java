7ed86700fbd6fc1394575fcf0a5b646536bba442
Jylan Payet <jylanpayet@gmail.com>
2021-01-05 22:30:49 +0100

    fetty

--- Payday/src/Vue/Aide.java
+++ Payday/src/Vue/Aide.java
@@ -22,22 +22,22 @@ public class Aide extends ImagePanel {
         JLabel l2 = new JLabel();
 
         l1.setText("<html><div class=\"container\">\n" +
-                "<h1>BIENVENUE DANS PAYDAY</h1>\n" +
-                "<h3>Tu as pour objectif de voler un maximum d'argent sans te faire attraper.</h3>\n" +
-                "<h3>8 missions périlleuse au sein de 8 banques différentes t'attendent.</h3></html>");
+                "<h1><i>BIENVENUE DANS PAYDAY</i></h1>\n" +
+                "<h3><i>Tu as pour objectif de voler un maximum d'argent sans te faire attraper.</i></h3>\n" +
+                "<h3><i>8 missions périlleuse au sein de 8 banques différentes t'attendent.</i></h3></html>");
         l1.setForeground(Color.white);
         l2.setText("<html><div class=\"container\">\n" +
-                "<h3>Au sein d'une mission, pour sauver un maximum d'argent il te faudra supprimer les cases de mêmes couleurs pour les faire disparaître</h3>\n" +
-                "<h3>Un sac est sauver si tu arrives à le faire atteindre la ligne du bas.</h3>\n" +
-                "<h3>Plus tu avances dans les niveaux plus la difficulté augmente  , une ligne apparaîtra au bout d'un certain  nombre de tour défini qui augmente.</h3>\n" +
-                "<h3>TU AS PLUSIEURS BONUS :</h3>\n" +
-                "<h3>-  Peinture, qui te permet de changer la couleur d'un bloc.</h3>\n" +
-                "<h3>- Fusée, qui te permet de faire disparaître tout une colonne.</h3>\n" +
-                "<h3>- Sauvetage, qui te permet de sauver un sac que tu n'aurais pas reussi à sauver.</h3>\n" +
+                "<h3><i>Au sein d'une mission, pour sauver un maximum d'argent il te faudra supprimer les cases de mêmes couleurs pour les faire disparaître</i></h3>\n" +
+                "<h3><i>Un sac est sauver si tu arrives à le faire atteindre la ligne du bas.</i></h3>\n" +
+                "<h3><i>Plus tu avances dans les niveaux plus la difficulté augmente  , une ligne apparaîtra au bout d'un certain  nombre de tour défini qui augmente.</i></h3>\n" +
+                "<h3><hr><i>Tu as plusieurs bonus :</i><hr></h3>\n" +
+                "<h3><i>- Peinture, qui te permet de changer la couleur d'un bloc.</i></h3>\n" +
+                "<h3><i>- Fusée, qui te permet de faire disparaître tout une colonne.</i></h3>\n" +
+                "<h3><i>- Sauvetage, qui te permet de sauver un sac que tu n'aurais pas reussi à sauver.</i></h3>\n" +
                 "<h3></h3>\n" +
-                "<h3>Comment les utiliser ?</h3>\n" +
-                "<h3>C'est très simple, clic sur un bonus pour le sélectionné  puis clic sur le bloc pour activer le bonus.</h3>\n" +
-                "<h3>Si tu change d'avis et que tu as déjà sélectionné un bonus , re-clic sur le bonus pour le desactiver.</h3>\n" +
+                "<h3><i><hr>Comment les utiliser ?<hr></i></h3>\n" +
+                "<h3><i>C'est très simple, clic sur un bonus pour le sélectionné  puis clic sur le bloc pour activer le bonus.</i></h3>\n" +
+                "<h3><i>Si tu change d'avis et que tu as déjà sélectionné un bonus , re-clic sur le bonus pour le desactiver.</i></h3>\n" +
                 "</div></html>");
         l2.setForeground(Color.white);
         this.setLayout(new GridBagLayout());
